a1f44b1d7b269740ca5828f9f8cf51a9151e7b5f
nghiale04 <155251067+nghiale04@users.noreply.github.com>
2025-04-05 19:51:43 +0700

    cros

--- hackathon_hcmute/src/main/java/com/andev03/hackathon/controller/AccountController.java
+++ hackathon_hcmute/src/main/java/com/andev03/hackathon/controller/AccountController.java
@@ -8,20 +8,18 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path="/api", produces = {MediaType.APPLICATION_JSON_VALUE})
 @AllArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class AccountController {
     private IAccontService iAccontService;
     @PostMapping("/register")
     public ResponseEntity<ResponseDto> registerAccount(@RequestBody AccountDto accountDto) {
         iAccontService.createAccount(accountDto);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto("200", "Success"));
+        return ResponseEntity.status(HttpStatus.CREATED).body(new ResponseDto("201", "Success"));
     }
     @PostMapping("/login")
     public ResponseEntity<AccountDto> loginAccount(@RequestBody LoginRequest loginRequest) {
